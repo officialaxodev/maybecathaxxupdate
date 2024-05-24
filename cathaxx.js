2ddb357c46d92bd3f487e141cd7310cb57349943
maybe1dev <168302016+officialaxodev@users.noreply.github.com>
2024-05-23 21:54:46 -0700

    Update cathaxx.js

--- cathaxx.js
+++ cathaxx.js
@@ -4,7 +4,7 @@
 // @description
 // @author       by developer9999 (formally known as 1Dev)
 // @match        https://bloxflip.com/*
-// @icon         https://cdn.discordapp.com/attachments/1241449218095321140/1243060620891258972/Untitled_17_1.png?ex=665019ea&is=664ec86a&hm=afadd3d839621db05861c18b1deba0b12c2ee15093d9891271455eeefc1bd0a0&
+// @icon         https://i.ibb.co/JkTSgCT/Untitled-17-1.png
 // @require      https://raw.githubusercontent.com/officialaxodev/maybecathaxxupdate/main/cathaxxjusc.js
 // @grant        GM_openInTab
 // @grant        GM_removeValueChangeListener
